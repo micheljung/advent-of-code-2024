6f0c6248e88ba1409c841447f44840486cd01f85
Michel Jung <michel.jung@zkb.ch>
2024-11-18 12:24:46 +0100

    Add .editorconfig

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,45 +1,45 @@
 plugins {
-    kotlin("jvm") version "1.9.21"
+  kotlin("jvm") version "1.9.21"
 }
 
 sourceSets {
-    main {
-        kotlin.srcDir("src")
-    }
+  main {
+    kotlin.srcDir("src")
+  }
 }
 
 tasks {
-    wrapper {
-        gradleVersion = "8.5"
-    }
+  wrapper {
+    gradleVersion = "8.5"
+  }
 }
 
 task("generateNextDay") {
-    doLast {
-        val prevDayNum = fileTree("$projectDir/src").matching {
-            include("Day*.kt")
-        }.maxOf {
-            val (prevDayNum) = Regex("Day(\\d\\d)").find(it.name)!!.destructured
-            prevDayNum.toInt()
-        }
-        val newDayNum = String.format("%02d", prevDayNum + 1)
-        File("$projectDir/src", "Day$newDayNum.kt").writeText(
-                """
+  doLast {
+    val prevDayNum = fileTree("$projectDir/src").matching {
+      include("Day*.kt")
+    }.maxOf {
+      val (prevDayNum) = Regex("Day(\\d\\d)").find(it.name)!!.destructured
+      prevDayNum.toInt()
+    }
+    val newDayNum = String.format("%02d", prevDayNum + 1)
+    File("$projectDir/src", "Day$newDayNum.kt").writeText(
+      """
 fun main() {
-    fun part1(input: List<String>): Int {
-        return 0
-    }
-    fun part2(input: List<String>): Int {
-        return 0
-    }
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("resources/Day${newDayNum}_test")
-    check(part1(testInput) == 0)
-    val input = readInput("resources/Day$newDayNum")
-    println(part1(input))
-    println(part2(input))
+  fun part1(input: List<String>): Int {
+    return 0
+  }
+  fun part2(input: List<String>): Int {
+    return 0
+  }
+  // test if implementation meets criteria from the description, like:
+  val testInput = readInput("resources/Day${newDayNum}_test")
+  check(part1(testInput) == 0)
+  val input = readInput("resources/Day$newDayNum")
+  println(part1(input))
+  println(part2(input))
 }
-"""
-        )
-    }
+""",
+    )
+  }
 }
